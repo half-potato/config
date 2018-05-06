356c300b75b9376c4ff22d61c999720172e2c9a7
Anon <tegra@fuckyou.com>
2018-05-06 14:37:50 -0700

    Updated install

--- src/install.sh
+++ src/install.sh
@@ -1,11 +1,27 @@
-read -p "Install packages? " -n 1 -r
+read -p "Install emacs? " -n 1 -r
 echo    
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
   sudo add-apt-repository ppa:kelleyk/emacs
   sudo apt-get update
-  sudo apt-get install vim-youcompleteme zsh vim emacs25
+  sudo apt-get install emacs25 
+  git clone https://github.com/syl20bnr/spacemacs.git ~/.emacs.d
+fi
+read -p "Install vim? " -n 1 -r
+echo    
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  sudo apt-get install vim-youcompleteme vim zsh fonts-powerline
   sudo chsh $USER -s /usr/bin/zsh
+fi
+read -p "Install zsh? " -n 1 -r
+echo    
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  sudo apt-get install zsh fonts-powerline
+  git clone https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
+  git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 fi
 read -p "Install handy packages? " -n 1 -r
 echo    
@@ -15,21 +31,3 @@ then
   sudo pip install numpy
   sudo pip3 install numpy
 fi
-read -p "Clone repos? " -n 1 -r
-echo    
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-  git clone https://github.com/syl20bnr/spacemacs.git ~/.emacs.d
-  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-  git clone https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
-fi
-
-read -p "Get colors? " -n 1 -r
-echo    
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-  mkdir -p ~/.vim/colors
-  cd ~/.vim/colors
-  wget https://raw.githubusercontent.com/ajmwagar/vim-deus/master/colors/deus.vim
-fi
-
